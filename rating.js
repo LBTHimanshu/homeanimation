26877f078a937d4f4d522919d788b8bfc99bcb54
Himanshu <himanshurajverma549@gmail.com>
2021-09-24 19:18:47 +0530

    button actions done

--- rating.js
+++ rating.js
@@ -1,32 +1,54 @@
-class RATING{
+class RATING {
     constructor() {
         this.stars = document.querySelectorAll(".star-item");
+        this.btn = document.querySelectorAll(".use-workflow-btn");
         this.rating = 0;
         this.init();
     }
-    
+
     init() {
         this.eventListener();
+        this.btnClick();
     }
-    
+
+    btnClick() {
+        this.btn.forEach((btn) => {
+            btn.addEventListener("click", (e) => {
+                let btnVal = e.currentTarget.dataset.btn;
+                if (btnVal === "first") {
+                    window.scrollTo({
+                        top: 398,
+                        behavior: 'smooth'
+                    });
+                }
+                if (btnVal === "second") {
+                window.scrollTo({
+                    top: 1742+398,
+                    behavior: 'smooth'
+                });
+            }
+            })
+        })
+    }
+
     eventListener() {
         this.stars.forEach(star => {
-            star.addEventListener("click" , (eve) => {
+            star.addEventListener("click", (eve) => {
                 let img = eve.currentTarget.firstChild;
                 let prevSibEle = star.previousElementSibling;
                 let nextSibEle = star.nextElementSibling;
                 if (prevSibEle) {
-                    while(prevSibEle){
+                    while (prevSibEle) {
                         let prevChild = prevSibEle.firstChild
                         if (!prevSibEle.firstElementChild.classList.contains("fill-star")) {
-                            prevChild.classList.add("fill-star"); 
+                            prevChild.classList.add("fill-star");
                             this.rating += 1;
                         }
                         prevSibEle = prevSibEle.previousElementSibling
                     }
                 }
                 if (nextSibEle && nextSibEle.firstElementChild.classList.contains("fill-star")) {
-                    while(nextSibEle){
+                    while (nextSibEle) {
                         let nextChild = nextSibEle.firstChild
                         if (nextSibEle.firstElementChild.classList.contains("fill-star")) {
                             nextChild.classList.remove("fill-star");
@@ -35,12 +57,12 @@ class RATING{
                         nextSibEle = nextSibEle.nextElementSibling;
                     }
                 }
-                
+
                 if (!img.classList.contains("fill-star")) {
                     img.classList.add("fill-star");
                     this.rating += 1;
                 }
-                else{
+                else {
                     this.rating -= 1;
                     img.classList.remove("fill-star");
                 }
