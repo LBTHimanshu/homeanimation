9e48bfbab2ff7efcd6ee209a3a8d84b0858e261d
Himanshu <himanshurajverma549@gmail.com>
2021-09-28 20:50:27 +0530

    completed for review

--- cms-filter.js
+++ cms-filter.js
@@ -44,11 +44,11 @@ class WORKFLOW {
     loadCards(data, categoryName, nicheName) {
         const CARDHTML = data.filter((card) => card.showOn === categoryName && card.showOnNiche === nicheName).map((card) => {
             if (card != null) {
-                return `<a href="${card.link}" role="listitem" class="workflow-card">
+                return `<a href="https://element5.webflow.io/workflow-category-items/${card.link}" role="listitem" class="workflow-card">
             <img src=${card.cardImgSrc} loading="lazy" alt="" class="workflow-card-img">
             <p class="para-16 workflow-card-head">${card.cardName}</p>
             <p class="para-16 workflow-card-para">${card.cardDetails}</p>
-            <p class="know-more-link card-link">KNOW MORE</p>
+            <p class="know-more-link card-link">Know More</p>
             </a>`.toString().split(',').join('');
             }
         });
--- rating.js
+++ rating.js
@@ -2,62 +2,69 @@ class RATING {
     constructor() {
         this.stars = document.querySelectorAll(".star-item");
         this.btn = document.querySelectorAll(".use-workflow-btn");
+        this.closeBtn = document.querySelector(".rating-success-close-btn");
+        this.ratingBtn = document.querySelector(".marketing-rating-btn");
         this.rating = 0;
         this.init();
     }
-
     init() {
         this.eventListener();
         this.btnClick();
     }
-
     btnClick() {
         this.btn.forEach((btn) => {
             btn.addEventListener("click", (e) => {
                 let btnVal = e.currentTarget.dataset.btn;
                 if (btnVal === "first") {
                     setTimeout(() => {
-                        let formTop = document.getElementsByClassName("section-anim-form")[0];
-                        let elDistanceToTop = window.pageYOffset + formTop.getBoundingClientRect().top;
+                        let formTop =
+                            document.getElementsByClassName("section-anim-form")[0];
+                        let elDistanceToTop =
+                            window.pageYOffset + formTop.getBoundingClientRect().top;
                         window.scrollTo({
                             top: elDistanceToTop - 110,
-                            behavior: 'smooth'
+                            behavior: "smooth",
                         });
                     }, 500);
-                }
-                else if (btnVal === "second") {
+                } else if (btnVal === "second") {
                     setTimeout(() => {
-                        let formTop = document.getElementsByClassName("section-anim-form-bottom")[0];
-                        let elDistanceToTop = window.pageYOffset + formTop.getBoundingClientRect().top;
+                        let formTop = document.getElementsByClassName(
+                            "section-anim-form-bottom"
+                        )[0];
+                        let elDistanceToTop =
+                            window.pageYOffset + formTop.getBoundingClientRect().top;
                         window.scrollTo({
                             top: elDistanceToTop - 110,
-                            behavior: 'smooth'
+                            behavior: "smooth",
                         });
                     }, 500);
                 }
-            })
-        })
+            });
+        });
     }
-
     eventListener() {
-        this.stars.forEach(star => {
+        this.closeBtn.addEventListener("click", () => this.ratingBtn.click())
+        this.stars.forEach((star) => {
             star.addEventListener("click", (eve) => {
                 let img = eve.currentTarget.firstChild;
                 let prevSibEle = star.previousElementSibling;
                 let nextSibEle = star.nextElementSibling;
                 if (prevSibEle) {
                     while (prevSibEle) {
-                        let prevChild = prevSibEle.firstChild
+                        let prevChild = prevSibEle.firstChild;
                         if (!prevSibEle.firstElementChild.classList.contains("fill-star")) {
                             prevChild.classList.add("fill-star");
                             this.rating += 1;
                         }
-                        prevSibEle = prevSibEle.previousElementSibling
+                        prevSibEle = prevSibEle.previousElementSibling;
                     }
                 }
-                if (nextSibEle && nextSibEle.firstElementChild.classList.contains("fill-star")) {
+                if (
+                    nextSibEle &&
+                    nextSibEle.firstElementChild.classList.contains("fill-star")
+                ) {
                     while (nextSibEle) {
-                        let nextChild = nextSibEle.firstChild
+                        let nextChild = nextSibEle.firstChild;
                         if (nextSibEle.firstElementChild.classList.contains("fill-star")) {
                             nextChild.classList.remove("fill-star");
                             this.rating -= 1;
@@ -65,18 +72,28 @@ class RATING {
                         nextSibEle = nextSibEle.nextElementSibling;
                     }
                 }
-
                 if (!img.classList.contains("fill-star")) {
                     img.classList.add("fill-star");
                     this.rating += 1;
-                }
-                else {
+                } else {
                     this.rating -= 1;
                     img.classList.remove("fill-star");
                 }
-            })
-        })
+            });
+        });
+    }
+    getRatingValue() {
+        return this.rating;
     }
 }
-
-new RATING;
+function ratingForm() {
+    const ratingObj = new RATING();
+    const $form = $("[rating-form='rating']");
+    $form.submit(function (evt) {
+        evt.preventDefault();
+        $form.append(
+            `<input style="height: 0; opacity: 0" type="name" name="user-rating" value="${ratingObj.getRatingValue()}" />`
+        );
+    });
+}
+ratingForm(); 
