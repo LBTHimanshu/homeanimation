45c87c23138dc766e52aea063a0e88887c91cea5
Himanshu <himanshurajverma549@gmail.com>
2021-10-01 19:11:00 +0530

    code cleaned

--- form-validation.js
+++ form-validation.js
@@ -41,92 +41,55 @@ class FROMVALIDATION {
 
     nameValidator(name) {
         name.addEventListener("blur", (evt) => {
-            let isTrue = false;
             if (name.value != "" && name.value != null && name.value.length > 3 && !name.value.match(/\d+/g)) {
                 this.name = name.value;
-                isTrue = true;
+                evt.target.classList.remove("invalid")
                 evt.target.classList.add("valid");
-                this.showIcon(evt, isTrue)
             }
             else {
                 evt.target.classList.remove("valid");
                 evt.target.classList.add("invalid");
-                this.showIcon(evt, isTrue)
             }
         })
     }
 
     emailValidator(email) {
         email.addEventListener("blur", (evt) => {
-            let isTrue = false;
             if (email.value != "" && email.value != null && email.value.length > 3 && email.value.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)$/)) {
                 this.email = email.value;
-                isTrue = true;
                 evt.target.classList.add("valid");
-                this.showIcon(evt, isTrue)
 
             }
             else {
                 evt.target.classList.remove("valid");
                 evt.target.classList.add("invalid");
-                this.showIcon(evt, isTrue)
             }
         })
     }
 
     numberValidator(number) {
         number.addEventListener("blur", (evt) => {
-            let isTrue = false;
             if (number.value != "" && number.value != null && number.value.length > 3 && number.value.match(/^\+91 +[0-9]{10}$/)) {
                 this.number = number.value;
-                isTrue = true;
                 evt.target.classList.add("valid");
-                this.showIcon(evt, isTrue)
             }
             else {
                 evt.target.classList.remove("valid");
                 evt.target.classList.add("invalid");
-                this.showIcon(evt, isTrue)
             }
         })
     }
 
     messageValidator(message) {
         message.addEventListener("blur", (evt) => {
-            let isTrue = false;
             if (message.value != "" && message.value != null && message.value.length > 10) {
                 this.message = message.value;                
-                isTrue = true;
                 evt.target.classList.add("valid");
-                this.showIcon(evt, isTrue)
 
             }
             else {
                 evt.target.classList.remove("valid");
                 evt.target.classList.add("invalid");
-                this.showIcon(evt, isTrue)
-            }
-        })
-    }
-
-    showIcon(eve, isTrue) {
-        let img = eve.target.parentElement.querySelectorAll("img");
-        img.forEach(img => {
-            if (isTrue) {
-                if (img.classList.contains("correct-img") && !img.classList.contains("active")) {
-                    img.classList.add("active");
-                }
-                if (img.classList.contains("wrong-img") && img.classList.contains("active")) {
-                    img.classList.remove("active");
-                }
-            }
-            else{
-                if (img.classList.contains("correct-img") && img.classList.contains("active")) {
-                    img.classList.remove("active");
-                }
-                if (img.classList.contains("wrong-img") && !img.classList.contains("active")) {
-                    img.classList.add("active");
-                }
             }
         })
     }
