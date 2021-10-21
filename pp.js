a66f9c02f88b6f0c732aa7424d9ac999ade7eb0e
Himanshu <himanshurajverma549@gmail.com>
2021-10-21 17:13:50 +0530

    final code completed

---
+++ pp.js
@@ -0,0 +1,39 @@
+var resizeTimer;
+function isInViewportCenter(element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
+
+function activateCategory(id) {
+    const tableContent = document.querySelectorAll('.sub-title');
+    tableContent.forEach(cat => {
+        if (cat.classList.contains("active")) {
+            cat.classList.remove("active");
+        }
+        if (cat.dataset.id == id) {
+            cat.classList.add("active");
+        }
+    })
+}
+
+$(window).on("scroll", function () {
+    clearTimeout(resizeTimer);
+    resizeTimer = setTimeout(function () {
+        [...$(".content-wrapper h2")].forEach((ele) => {
+            if (isInViewportCenter(ele)) {
+                if (
+                    ele.getBoundingClientRect().top >= 0 &&
+                    ele.getBoundingClientRect().top <= 125
+                ) {
+                    activateCategory(ele.id)
+                }
+            }
+        });
+    }, 250);
+});
