4c13074a38dd559db6caf64caec733395d41802b
Himanshu <himanshurajverma549@gmail.com>
2021-11-22 18:35:44 +0530

    made changes for animation

--- animation.js
+++ animation.js
@@ -7,6 +7,7 @@ class AnimateBlock {
       this.topVal = this.imgChildren[0].height;
       this.movedVal = 0;
       this.currImg = 1;
+      this.allMoved = false;
       this.action = "moveup";
     }
     handleAni() {
@@ -18,18 +19,13 @@ class AnimateBlock {
         this.currImg === this.childImgLength ||
         (this.currImg < this.childImgLength && this.currImg > 0)
       ) {
-        this.action = "movedown";
-        this.movedVal = this.movedVal - this.topVal;
-        if (this.movedVal < 0) {
-          this.action = "moveup";
           this.currImg = 1;
+          this.allMoved = true;
           this.movedVal = 0;
-          this.handleAni();
-        } else {
-          this.currImg = this.currImg - 1;
-          this.positionAni();
+          // this.positionAni();
+          this.allMoved = true;
         }
-      } else {
+     else {
         this.action = "moveup";
         this.currImg = 1;
         this.movedVal = 0;
@@ -37,11 +33,23 @@ class AnimateBlock {
       }
     }
     positionAni() {
-      gsap.to(this.block, {
+      // if(!this.allMoved){
+        // console.log("if")
+        gsap.to(this.block, {
         y: -this.movedVal,
         duration: this.aniDuration,
         ease: "Power1.easeInOut",
-      });
+      })
+    // }
+      // else{
+      //   console.log("else")
+      //   // gsap.to(".image-wrapper", {
+      //   //   y: 0,
+      //   //   duration: 0,
+      //   //   ease: "Power1.easeInOut",
+      //   // })
+      //   this.allMoved = true;
+      // }
     }
   }
   class ANIMATE {
@@ -50,7 +58,11 @@ class AnimateBlock {
       this.randomVal = 0;
       this.duration = duration;
       this.indexOfwrapper = 0;
-      this.checkRandom = [];
+      // this.checkRandom = [];
+      this.oddNum = [];
+      this.evenNum = [];
+      this.evenFirst = true;
+      this.sendWrapVal = 0;
       this.companyLogos = companyLogos;
       this.init();
     }
@@ -87,20 +99,107 @@ class AnimateBlock {
       }
     }
     startCounter() {
-      setInterval(() => {
-        this.checkIfNumRep();
+      this.storeNumbers(this.wrapper.length);
+      this.timerId = setInterval(() => {
+        // this.checkIfNumRep();
+        this.getNumber();
         if (Array.isArray(this.aniFuncs) && this.aniFuncs[this.randomVal]) {
-          this.aniFuncs[this.randomVal].handleAni();
+          if(!this.checkIfMoved()){
+            console.log("first time")
+            this.aniFuncs[this.randomVal].handleAni();
+          } 
+          else{
+            if(this.checkIfAllMoved()){
+              console.log("reset")
+              this.restart()
+            }
+            else{
+              this.checkAndHandleAni();
+            }
+          }
         }
       }, 1000);
     }
-    checkIfNumRep() {
-      this.lastCalled = this.randomVal;
-      this.randomVal = this.setRandomVal(this.wrapper.length);
-      if (this.lastCalled && this.randomVal === this.lastCalled) {
-        this.checkIfNumRep();
+    checkAndHandleAni(){
+      if(!this.checkIfMoved()){
+        this.getNumber();
+        this.aniFuncs[this.randomVal].handleAni();
+      }
+      else{
+        this.randomVal % 2 == 0 ? this.evenNum.splice(this.evenNum.indexOf(this.randomVal, 1)) : this.oddNum.splice(this.oddNum.indexOf(this.randomVal, 1));
+        this.checkAndHandleAni();
       }
     }
+    checkIfMoved(){
+      return (this.aniFuncs[this.randomVal].allMoved)
+    }
+    checkIfAllMoved(){
+      const aniFound = this.aniFuncs.find(obj => obj.allMoved === false);
+      if(aniFound) {
+        return false;
+      }
+      return true;
+    }
+    stopCounter(){
+      this.timerId && clearInterval(this.timerId)
+    }
+    getNumber() {
+      if(this.evenFirst){
+        if (this.sendWrapVal != this.evenNum.length) {
+          this.randomVal = this.evenNum[this.sendWrapVal];
+          this.sendWrapVal++;
+        }
+        else{
+          this.sendWrapVal = 0;
+          this.evenFirst = false;
+          this.getNumber();
+        }
+      }
+      else{
+        if (this.sendWrapVal != this.oddNum.length) {
+          this.randomVal = this.oddNum[this.sendWrapVal];
+          this.sendWrapVal++;
+        }
+        else{
+          this.sendWrapVal = 0;
+          this.evenFirst = true;
+          this.getNumber();
+        }
+      }
+      if (this.oddNum.length === 0 && this.evenNum.length === 0) {
+        console.log()
+        this.restart();
+      }
+    }
+    restart(){
+      this.stopCounter(this.timerId);
+      this.aniFuncs.forEach(obj => {
+        obj.allMoved = false
+      })
+        gsap.to(".image-wrapper", {
+          y: 0,
+          duration: 0,
+          ease: "Power1.easeInOut",
+        })
+        this.startCounter();
+    }
+    storeNumbers(length){
+      for (let num = 0; num < length; num++) {
+        if ((num % 2) != 1) {
+          this.evenNum.push(num);
+      }
+      else {
+          this.oddNum.push(num);
+      }
+    }
+  }
+    // checkIfNumRep() {
+    //   this.lastCalled = this.randomVal;
+    //   this.randomVal = this.setRandomVal(this.wrapper.length);
+    //   if (this.lastCalled && this.randomVal === this.lastCalled) {
+    //     this.checkIfNumRep();
+    //   }
+    // }
     setRandomVal(length) {
       let num = Math.floor(Math.random() * length);
       if (!this.checkRandom.includes(num) && this.checkRandom.length <= length) {
